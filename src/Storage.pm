5e224d6dcaf2da599a3e879409e472adf5daa1f2
mamema <mamema2003@hotmail.com>
2021-03-03 11:14:55 +0100

    Update Storage.pm

--- src/Storage.pm
+++ src/Storage.pm
@@ -1321,8 +1321,7 @@ sub refreshTracks
 			    	rollback($dbh); #just die if rollback is failing
 			    };
 			}		
-			$sql = "CREATE temp table temp_track_statistics as select tracks.url,tracks.urlmd5,tracks.musicbrainz_id from tracks join track_statistics on tracks.musicbrainz_id=track_statistics.musicbrainz_id where track_statistics.musicbrainz_id is not null and track_statistics.urlmd5!=tracks.urlmd5 and track_statisticc
-s.filesize != tracks.filesize";
+			$sql = "CREATE temp table temp_track_statistics as select tracks.url,tracks.urlmd5,tracks.musicbrainz_id from tracks join track_statistics on tracks.musicbrainz_id=track_statistics.musicbrainz_id where track_statistics.musicbrainz_id is not null and track_statistics.urlmd5!=tracks.urlmd5 and track_statistics.filesize = tracks.filesize";
 			$sth = $dbh->prepare( $sql );
 			$count = 0;
 			eval {
